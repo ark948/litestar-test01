85a0aade407a33a4235c9ac8a6b57f4044824a6f
ark948 <armankarimi948@gmail.com>
2025-02-10 21:40:27 +0330

    Modify data

--- backend/source/main.py
+++ backend/source/main.py
@@ -1,5 +1,5 @@
-from litestar import Litestar, get, post
-from litestar.exceptions import HTTPException
+from litestar import Litestar, get, post, put
+from litestar.exceptions import HTTPException, NotFoundException
 from dataclasses import dataclass
 from typing import Any
 
@@ -11,7 +11,11 @@ class TodoItem:
     done: bool
 
 
-TODO_LIST: list[TodoItem] = []
+TODO_LIST: list[TodoItem] = [
+    TodoItem(title="Start writing TODO list", done=True),
+    TodoItem(title="???", done=False),
+    TodoItem(title="Profit", done=False),
+]
 
 
 
@@ -40,8 +44,26 @@ async def add_item(data: TodoItem) -> list[TodoItem]:
 
 
 
+def get_todo_by_title(todo_name) -> TodoItem:
+    for item in TODO_LIST:
+        if item.title == todo_name:
+            return item
+    raise NotFoundException(detail=f"TODO {todo_name!r} not found")
+
+
+
+@put("/{item_title:str}")
+async def update_item(item_title: str, data: TodoItem) -> list[TodoItem]:
+    todo_item = get_todo_by_title(item_title)
+    todo_item.title = data.title
+    todo_item.done = data.done
+    return TODO_LIST
+
+
+
+
 app = Litestar(
-    [index, get_list, add_item]
+    [index, get_list, add_item, update_item]
 )
 
 
